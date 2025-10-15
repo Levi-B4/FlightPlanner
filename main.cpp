5c5547c243b427300a5928d7d2743dd1c9762c5a
Levi Butler <levi44b@gmail.com>
2025-10-15 18:37:00 -0400

    added text to see where in the program the segfault happens

--- main.cpp
+++ main.cpp
@@ -5,9 +5,11 @@
 int main(int argc, char* argv[])
 {
     if(argc != 4){
-        std::cout << "Error: There were " << argc << " arguments given instead of 4.";
+        std::cout << "Error: There were " << argc << " arguments given instead of 4." << std::endl;
         return -1;
     }
+
+    std::cout << "Welcome to the flight planner!" << std::endl;
 
     DSString dataPath = argv[1];
     DSString plansPath = argv[2];
@@ -17,6 +19,8 @@ int main(int argc, char* argv[])
 
     flights.createFlightList(dataPath);
     flights.planFlights(plansPath, outputPath);
+
+    std::cout << "You can find the data in \"" << outputPath << "\", goodbye!" << std::endl;
 
     return 0;
 }
