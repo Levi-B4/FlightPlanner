b709da8a0f4d5b41921ca88a9c33f9d93883aff9
Levi Butler <levi44b@gmail.com>
2025-10-15 18:25:12 -0400

    Added handling of improper amount of arguments given

--- main.cpp
+++ main.cpp
@@ -4,6 +4,11 @@
 
 int main(int argc, char* argv[])
 {
+    if(argc != 4){
+        std::cout << "Error: There were " << argc << " arguments given instead of 4.";
+        return -1;
+    }
+
     DSString dataPath = argv[1];
     DSString plansPath = argv[2];
     DSString outputPath = argv[3];
